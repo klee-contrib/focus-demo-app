e484cf92e20af1f7055d99df1b70050843892d7d
Pierre Besson <pierre.besson7@gmail.com>
2016-01-29 10:51:31 +0100

    [router] Correction

--- app/router/formation-router.js
+++ app/router/formation-router.js
@@ -2,6 +2,17 @@ import React from 'react';
 import application from 'focus-core/application';
 import router from 'focus-core/router';
 
+function MonComposant(props){
+  return <div>Test</div>;
+}
+
+const MonComposant2 = React.createClass({
+  render(){
+    const {id, name} = this.props;
+    return <h2>Test2 mon id {id} et mon nom: {name}</h2>;
+  }
+});
+
 export default router.extend({
     log: true,
     beforeRoute() {
@@ -9,11 +20,16 @@ export default router.extend({
     },
     routes: {
         '': 'demoRouteHandler',
-        'demo': 'demoRouteHandler'
+        'demo': 'demoRouteHandler',
+        'demo/:id': 'detailDemoHandler'
     },
     demoRouteHandler() {
         console.log('ROUTER: DEMO');
         //Page content vient du router
-        this._pageContent(props => <h2>Bienvenue à la formation du 29/11/2016</h2>);
+        this._pageContent(MonComposant2);
+    },
+    detailDemoHandler(id){
+        this._pageContent(MonComposant2, {props: {id: id, name: 'Pierre'}});
+        // <MonComposant2  id={id}/>
     }
 });
