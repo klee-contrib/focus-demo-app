27a713944ded7e4e45ef19f7a8f345814d92bd6e
bernardstanislas <bernardstanislas@gmail.com>
2016-02-12 18:57:02 +0100

    Bulk insertion

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('focus-demo-app', '', '', {
     dialect: 'sqlite',
-    storage: 'lol.db',
+    storage: ':memory:',
     logging: false
 });
 
--- api/populate.js
+++ api/populate.js
@@ -1,3 +1,4 @@
+const Promise = require('bluebird');
 const sequelize = require('./db').sequelize;
 const Movie = require('./db').Movie;
 const Person = require('./db').Person;
@@ -10,33 +11,34 @@ sequelize.sync()
     Promise.all(Movie.bulkCreate(movies), Person.bulkCreate(persons));
 })
 .then(() => {
-    Promise.all(movies.map(movie => MoviePerson.bulkCreate((movie.actors || []).map(actor => ({
-        movieCode: movie.code,
-        personCode: actor.code,
-        name: actor.name,
-        type: 'actor',
-        role: actor.role,
-        leadActor: actor.leadActor
-    })).concat((movie.directors || []).map(director => ({
-        movieCode: movie.code,
-        personCode: director.code,
-        name: director.name,
-        type: 'director'
-    }))).concat((movie.producers || []).map(producer => ({
-        movieCode: movie.code,
-        personCode: producer.code,
-        name: producer.name,
-        type: 'producer'
-    }))).concat((movie.writers || []).map(writer => ({
-        movieCode: movie.code,
-        personCode: writer.code,
-        name: writer.name,
-        type: 'writer'
-    }))))));
+    const roles = ['actor', 'director', 'producer', 'writer'];
+    return Promise.resolve(roles.reduce((roleAcc, role) => {
+        return roleAcc.concat(movies.reduce((movieAcc, movie) => {
+            if (!movie[`${role}s`]) return movieAcc;
+            return movieAcc.concat(movie[`${role}s`].map(person => ({
+                movieCode: movie.code,
+                personCode: person.code,
+                name: person.name,
+                type: role,
+                role: person.role,
+                leadActor: person.leadActor
+            })));
+        }, []));
+    }, []));
+})
+.then(persons => {
+    const bulks = [];
+    const bulkSize = 5000;
+    const bulkCount = Math.ceil(persons.length / bulkSize);
+    for (var i = 0; i < bulkCount; i++) {
+        bulks.push({start: bulkSize * i, end: bulkSize * (i + 1)});
+    }
+    return Promise.each(bulks, (bulk, index) => {
+        MoviePerson.bulkCreate(persons.slice(bulk.start, bulk.end));
+    });
 })
 .then(() => {
-    Movie.findAll({include: [Person]}).then(data => {
-        const movie = data[0];
-        movie.getPeople().then(console.log);
+    Movie.find({where: {code: 1791}, include: [Person]}).then(movie => {
+        movie.getPeople().then(people => console.log(people.map(person => person.get({plain: true}))));
     });
 })
