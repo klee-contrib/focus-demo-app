61452d2debb8d3508fe28142d7ccd56bc3441883
Pierre Besson <pierre.besson7@gmail.com>
2016-02-24 12:12:53 +0100

    [change the group color]

--- app/stores/dx-logger.js
+++ app/stores/dx-logger.js
@@ -3,6 +3,7 @@ import CoreStore from 'focus-core/store/CoreStore';
 import {isObject, isArray, isFunction} from 'lodash/lang';
 
 const STORE_LOGGER = 'DX_STORE_LOGGER';
+const CONSOLE_STYLE ='color: blue;';
 
 // Creates a logger which listen to all the dispatched information, and uses a store list
 // The dispatcher should be a flux/dispatcher
@@ -21,7 +22,7 @@ const createStoreLogger = (dispatcher, getStores) => {
       throw new Error(`${STORE_LOGGER} : you should provide a list of stores to read see focus-core/store/CoreStore.prototype._instances.`)
     }
     const {type, data} = transferInfo.action;
-    console.groupCollapsed(`${STORE_LOGGER} : action ${type} ${Object.keys(data).join('')}`);
+    console.groupCollapsed(`%c ${STORE_LOGGER} : action ${type} ${Object.keys(data).join('')}`, CONSOLE_STYLE);
       console.log('action dispatcher source', `${transferInfo.source} ${type} ${Object.keys(data).join('')} `, data)
       console.groupCollapsed(`${STORE_LOGGER} stores values`);
         Object.keys(stores).reduce((res, current) => {
