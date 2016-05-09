faa4efc608ddd1546c5d3a4a1e236ef25fdfc10e
GuenoleK <kikabouguenole@gmail.com>
2016-05-09 19:49:21 +0200

    Minor changes

--- app/router2/routes.js
+++ app/router2/routes.js
@@ -3,7 +3,8 @@ import { Route, IndexRoute } from 'react-router';
 import Home from '../views/home';
 import MasterdataTemplate from '../views/masterdata';
 import MasterdataCountries from '../views/masterdata/country';
-
+import ErorView from '../views/test/error';
+import MovieDetailView from '../views/movie/detail';
 
 // Gotta do a function to get the case as the first router does
 const MasterdataTemplateCountry = React.createClass({
@@ -36,6 +37,37 @@ const MasterdataTemplateGender = React.createClass({
     }
 });
 
+// const MasterdataTemplateRender = React.createClass({
+//     render() {
+//         const path = window.location.pathname;
+//         let ref;
+//         let ReferenceComponent;
+//
+//         switch (path) {
+//             case '/admin/masterdata/countries':
+//                 ref = 'countries';
+//                 ReferenceComponent = MasterdataCountries;
+//                 break;
+//             case '/admin/masterdata/movietype':
+//                 ref = 'movietype';
+//                 ReferenceComponent = undefined;
+//                 break;
+//             case '/admin/masterdata/gender':
+//                 ref = 'gender';
+//                 ReferenceComponent = undefined;
+//                 break;
+//             default:
+//                 ReferenceComponent = undefined;
+//         };
+//
+//         return(
+//             <div>
+//                 <MasterdataTemplate reference={ref} ReferenceComponent={ReferenceComponent}/>
+//             </div>
+//         );
+//     }
+// });
+
 module.exports = (
     <div>
         <Route path='/' component={Home}/>
@@ -44,5 +76,6 @@ module.exports = (
         <Route path='/admin/masterdata/countries' component={MasterdataTemplateCountry}/>
         <Route path='/admin/masterdata/movietype' component={MasterdataTemplateMovie}/>
         <Route path='/admin/masterdata/gender' component={MasterdataTemplateGender}/>
+        <Route path='movies/:id' component={MovieDetailView}/>
     </div>
 );
