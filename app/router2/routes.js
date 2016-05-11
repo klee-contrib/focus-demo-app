8843e690fef51b88732c86015284181453710549
GuenoleK <kikabouguenole@gmail.com>
2016-05-11 12:46:47 +0200

    Upating the router

--- app/router2/routes.js
+++ app/router2/routes.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import AppLayout from '../components/layout'
 import HomeRoute from './home-route';
-import MasterdataRoutes from './masterdataRoutes';
+import MasterdataRoutes from './masterdata-routes';
+import MoviesRoutes from './movies-route';
 
 export default {
     path: `${__BASE_URL__}`,
     indexRoute: { onEnter: ({ params }, replace) => replace(`${__BASE_URL__}home`) },
     component: AppLayout,
-    childRoutes: [...HomeRoute, ...MasterdataRoutes]
+    childRoutes: [...HomeRoute, ...MasterdataRoutes, ...MoviesRoutes]
 };
