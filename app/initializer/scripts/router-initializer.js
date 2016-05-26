6c37d973523b8ca6fa271250ea73ac75248b1627
GuenoleK <kikabouguenole@gmail.com>
2016-05-26 10:27:23 +0200

    Adding Backbone navigation

--- app/initializer/scripts/router-initializer.js
+++ app/initializer/scripts/router-initializer.js
@@ -1,8 +1,9 @@
 import {setRouterFunction} from 'focus-core/router/router';
 import {setNavigationFunctions} from 'focus-core/history';
-import {history} from 'backbone';
+import Backbone, {history} from 'backbone';
 
 export default () => {
+    setRouterFunction(Backbone.Router);
     setNavigationFunctions(history.navigate, history.history.back);
 
 }
