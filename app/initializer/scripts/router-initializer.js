3fac2a00616032b5bb763426eb5649dd5f9762d0
GuenoleK <kikabouguenole@gmail.com>
2016-05-31 18:42:57 +0200

    Adding the backbone start in the set arguments

--- app/initializer/scripts/router-initializer.js
+++ app/initializer/scripts/router-initializer.js
@@ -3,5 +3,5 @@ import {setNavigationFunctions} from 'focus-core/history';
 import Backbone from 'backbone';
 
 export default () => {
-    setNavigationFunctions(Backbone.history.navigate.bind(Backbone.history), Backbone.history.history.back.bind(Backbone.history.history));
+    setNavigationFunctions(Backbone.history.navigate.bind(Backbone.history), Backbone.history.history.back.bind(Backbone.history.history), Backbone.history.start.bind(Backbone.history));
 }
