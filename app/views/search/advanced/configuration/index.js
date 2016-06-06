3832a3c2f5e164dceb4e065ac6fa4ee9fb09981e
Thomas <tgallon@gmail.com>
2016-06-06 17:35:34 +0200

    add action in advanced search conf

--- app/views/search/advanced/configuration/index.js
+++ app/views/search/advanced/configuration/index.js
@@ -6,12 +6,20 @@ import cartridgeConfiguration from './cartridge';
 import lineMapper from '../../lines/mapper';
 import onLineClick from '../../lines/line-click';
 import {scopesConfig} from '../../../../config/scopes';
+import actionBuilder from 'focus-core/search/action-builder';
+
+import searchBuiltInStore from 'focus-core/search/built-in-store';
+const advancedSearchStore = searchBuiltInStore.advancedSearchStore;
 
 export const configuration = {
+    action: actionBuilder({
+        service,
+        identifier: advancedSearchStore.identifier,
+        getSearchOptions: () => advancedSearchStore.getValue.call(advancedSearchStore) // Binding the store in the function call
+    }),
     onLineClick,
     isSelection: true,
     cartridgeConfiguration,
-    service,
     lineComponentMapper: lineMapper,
     groupMaxRows: 5, // ne fonctionne pas, n'est pas renvoyé dans la config du service search.
     scopesConfig: scopesConfig
