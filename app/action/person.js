39ee881ab37e2e8657cfcff21acf13e9b10d8ef5
Thomas <tgallon@gmail.com>
2016-08-09 18:10:02 +0200

    correct actionbuilder name

--- app/action/person.js
+++ app/action/person.js
@@ -1,26 +1,25 @@
 import personServices from '../services/person';
-import actionBuilder from 'focus-core/application/action-builder';
-import {actionBuilder as actionBuilder2} from 'focus-graph/actions/entity-actions-builder';
+import {actionBuilder} from 'focus-graph/actions/entity-actions-builder';
 
 //---------- IDENTITY
-const _loadIdentityAction = actionBuilder2({names: ['person'], type:'load', service: personServices.loadPerson});
+const _loadIdentityAction = actionBuilder({names: ['person'], type:'load', service: personServices.loadPerson});
 export const loadIdentityTypes = _loadIdentityAction.types;
 export const loadIdentityAction = _loadIdentityAction.action;
 
-const _saveIdentityAction = actionBuilder2({names: ['person'], type:'save', service: personServices.updatePersonIdentity});
+const _saveIdentityAction = actionBuilder({names: ['person'], type:'save', service: personServices.updatePersonIdentity});
 export const saveIdentityTypes = _saveIdentityAction.types;
 export const saveIdentityAction = _saveIdentityAction.action;
 
 //---------- BIOGRAPHY
-const _loadBiographyAction = actionBuilder2({names: ['person'], type:'load', service: personServices.loadPerson});
+const _loadBiographyAction = actionBuilder({names: ['person'], type:'load', service: personServices.loadPerson});
 export const loadBiographyTypes = _loadBiographyAction.types;
 export const loadBiographyAction = _loadBiographyAction.action;
 
-const _saveBiographyAction = actionBuilder2({names: ['person'], type:'save', service: personServices.updatePersonBiography});
+const _saveBiographyAction = actionBuilder({names: ['person'], type:'save', service: personServices.updatePersonBiography});
 export const saveBiographyTypes = _saveBiographyAction.types;
 export const saveBiographyAction = _saveBiographyAction.action;
 
 //---------- MOVIE LINKS
-const _movieLinksAction = actionBuilder2({names: ['movieLink'], type:'load', service: personServices.loadPersonMovies});
+const _movieLinksAction = actionBuilder({names: ['movieLink'], type:'load', service: personServices.loadPersonMovies});
 export const loadMovieLinksTypes = _movieLinksAction.types;
 export const loadMovieLinksAction = _movieLinksAction.action;
